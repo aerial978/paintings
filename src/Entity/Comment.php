0e6a5a46975a39890d95e29d022385ceb0c26c19
Michel Hathier <mhathiergmail.com>
2022-10-31 21:14:52 +0100

    ajout entités + tests unitaires

---
+++ src/Entity/Comment.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    private ?painting $painting = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    private ?Blogpost $blogpost = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getPainting(): ?painting
+    {
+        return $this->painting;
+    }
+
+    public function setPainting(?painting $painting): self
+    {
+        $this->painting = $painting;
+
+        return $this;
+    }
+
+    public function getBlogpost(): ?Blogpost
+    {
+        return $this->blogpost;
+    }
+
+    public function setBlogpost(?Blogpost $blogpost): self
+    {
+        $this->blogpost = $blogpost;
+
+        return $this;
+    }
+}
