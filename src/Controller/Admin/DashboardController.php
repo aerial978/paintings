51882ec1d55b17f9ac7e853bca06b4fb207c6754
Michel Hathier <mhathiergmail.com>
2022-11-17 21:16:03 +0100

    installation EasyAdmin4 + customisation dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('<h3 class="text-center">Painting.</h3>')
+            ->renderContentMaximized();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
